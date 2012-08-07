3cc5d0d1c4ce60735d3f375c47cdde3c612e0c44
Emiliano Lozano <emiliano@minidonations.org>
2012-08-06 23:17:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd29117340629c0caaf62e6141957730dc65eafbc24468fb746fe315a76f71b191b70b456f539f400637d4457cc47b946dffcc23cca4a83bd3135805fd0055731'
